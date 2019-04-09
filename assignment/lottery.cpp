70088c95c4b16310bc54c285d370fac88e3cef78
LeonNepelius <leon@nepelius.at>
2019-04-09 19:15:06 +0200

    5 test passing

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -12,6 +12,8 @@
 */
 #include "lottery.h"
 #include "general.h"
+#include <string.h> // for strtok to split the string to substring with separator
+#include <stdlib.h> // for atoi to work (converts string to int)
 #include <stdio.h>
 
 #define 	UUID_LEN   40
@@ -19,16 +21,45 @@
 #define 	MAX_LINE_LEN   (UUID_LEN + 1 + MAX_TIP_LEN + 1)
 
 static FILE* stream;
+static char separator;
 
 bool init_lottery(const char *csv_file, char csv_separator)
 {
   stream = fopen(csv_file, "r");
+  separator = csv_separator;
   if (stream == 0) return false;
   return true;
 }
 
 bool get_tip(int tip_number, int tip[TIP_SIZE])
 {
+    if (tip_number < 0 || tip_number > 45) return false;
+    int count = 0;
+    char line[100];
+    while ( fgets(line, 100, stream) != NULL) // read a line
+    {
+        if (count == tip_number)
+        {
+            if (line[strlen(line)-1] == '\n') line[strlen(line)-1] = '\0'; // need to remove \n (new line) to get the last number without \n
+            count = 0;
+            char str[2];
+            str[0] = separator;
+            str[1] = '\0';
+            char* ptr = strtok(line, str); // gets the first string before separator
+            while(ptr != NULL && count < TIP_SIZE)
+            {
+                ptr = strtok(NULL, str); // gets next string
+                tip[count] = atoi(ptr); // converts the string to an int
+                count++;
+            }
+            rewind(stream); // sets current to 0 to start at the beginning next time
+            return true;
+        }
+        else
+        {
+            count++;
+        }
+    }
   return false;
 }
 
